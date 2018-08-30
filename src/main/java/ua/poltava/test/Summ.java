478f56c55baae254b0dc6be3fd0b396e5895202d
Mykola <612284@gmail.com>
2018-08-31 01:00:53 +0300

    add class Summ

---
+++ src/main/java/ua/poltava/test/Summ.java
@@ -0,0 +1,15 @@
+package main.java.ua.poltava.test;
+
+public class Summ {
+    private int a;
+    private int b;
+
+    public Summ(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int summ() {
+        return a + b;
+    }
+}
